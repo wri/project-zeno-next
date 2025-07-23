12db0dc41cfa26627ab84ef6b0c33f85d03bf7fe
Will Rynearson <will@developmentseed.org>
2025-07-23 17:46:05 +0100

    create error component custom styling

--- app/ChatPanel.tsx
+++ app/ChatPanel.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import {
-  Flex,
-  Box,
-} from "@chakra-ui/react";
+import { Flex, Box, Button } from "@chakra-ui/react";
+import { Toaster, toaster } from "@/app/components/ui/toaster";
 import ChatInput from "./components/ChatInput";
 import ChatMessages from "./components/ChatMessages";
 import ChatPanelHeader from "./ChatPanelHeader";
 
 function ChatPanel() {
-  
   return (
     <Flex minH="100%" maxH="100%" flexDir="column" gridArea="chat">
       <ChatPanelHeader />
@@ -18,10 +15,26 @@ function ChatPanel() {
         pb="2"
         position="relative"
         flex="1"
-        flexDir="column" 
+        flexDir="column"
         height="100%"
         overflow="auto"
       >
+        <Toaster />
+        <Button
+          variant="outline"
+          onClick={() =>
+            toaster.create({
+              title: "This is a description of the problem",
+              description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum dolor.",
+              type: "error",
+              closable: true,
+              duration: 600000,
+            })
+          }
+        >
+          Hello
+        </Button>
         <Box flex="1" overflowY="auto" height="100%" mx="-4" px="4" pb="8">
           <ChatMessages />
         </Box>
