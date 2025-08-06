1f9a305c40d4e57f932bca7dd9f5dfade236ddbf
LanesGood <Lane@developmentseed.org>
2025-08-05 22:32:29 -0700

    Remove unused dialog footer. Update styles

--- app/components/WelcomeModal.tsx
+++ app/components/WelcomeModal.tsx
@@ -10,12 +10,13 @@ import {
   Textarea,
   ButtonGroup,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { usePromptStore } from "@/app/store/promptStore";
 import useChatStore from "@/app/store/chatStore";
 import { ArrowBendRightUpIcon, SparkleIcon } from "@phosphor-icons/react";
 
 const WelcomeModal = () => {
+  const ref = useRef<HTMLInputElement>(null);
   const { prompts, fetchPrompts } = usePromptStore();
   const { sendMessage, isLoading } = useChatStore();
   const [isOpen, setIsOpen] = useState(true);
@@ -65,122 +66,126 @@ const WelcomeModal = () => {
       onOpenChange={() => setIsOpen(isOpen)}
       placement="center"
       size="lg"
+      initialFocusEl={() => ref.current}
     >
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
           <Dialog.Content bg="bg.muted" padding="2">
             <Dialog.Header justifyContent="center" alignItems="center">
-              <Dialog.Title fontWeight="500" fontSize="xl">
+              <Dialog.Title fontWeight="normal" fontSize="2xl" m={0}>
                 Welcome to <strong>NatureWATCH</strong>
               </Dialog.Title>
             </Dialog.Header>
-            <Dialog.Body display="flex" flexDirection="column" gap="32px">
-              <Dialog.Description>
-                Our AI-powered nature monitoring tool understands plain
-                language, so no technical jargon required! Ask about land cover
-                change, forest loss, or biodiversity risks in the areas you care
-                about and quickly generate powerful insights. Whether
-                you&apos;re a policymaker, scientist, or community advocate,
-                we&apos;ll help you find the right data and make sense of it,
-                too.
-              </Dialog.Description>
-              <Flex
-                flexDir="column"
-                position="relative"
-                m={0}
-                p={4}
-                bg="bg.subtle"
-                borderRadius="md"
-                borderWidth="1px"
-                className="group"
-                transition="all 0.32s ease-in-out"
-                _active={{
-                  bg: "white",
-                  borderColor: "blue.900",
-                }}
-                _focusWithin={{
-                  bg: "white",
-                  borderColor: "blue.900",
-                }}
-              >
-                <Textarea
-                  aria-label="Ask a question..."
-                  placeholder="Ask a question..."
-                  fontSize="sm"
-                  autoresize
-                  maxH="10lh"
-                  border="none"
-                  p={0}
-                  value={inputValue}
-                  onChange={(e) => setInputValue(e.target.value)}
-                  onKeyUp={handleKeyUp}
-                  disabled={disabled}
-                  _disabled={{
-                    opacity: 1,
+            <Dialog.Body display="flex" flexDirection="column" gap="12">
+              <Flex display="flex" flexDirection="column" gap="8">
+                <Dialog.Description fontSize="xs">
+                  Our AI-powered nature monitoring tool understands plain
+                  language, so no technical jargon required! Ask about land
+                  cover change, forest loss, or biodiversity risks in the areas
+                  you care about and quickly generate powerful insights. Whether
+                  you&apos;re a policymaker, scientist, or community advocate,
+                  we&apos;ll help you find the right data and make sense of it,
+                  too.
+                </Dialog.Description>
+                <Flex
+                  flexDir="column"
+                  position="relative"
+                  m={0}
+                  p={4}
+                  bg="bg"
+                  rounded="xl"
+                  borderWidth="1px"
+                  className="group"
+                  transition="all 0.32s ease-in-out"
+                  _active={{
+                    bg: "white",
+                    borderColor: "blue.900",
                   }}
-                  _focus={{
-                    outline: "none",
+                  _focusWithin={{
+                    bg: "white",
+                    borderColor: "blue.900",
                   }}
-                />
-                <Button
-                  p="0"
-                  ml="auto"
-                  borderRadius="full"
-                  colorPalette="blue"
-                  bg="blue.900"
-                  _disabled={{
-                    opacity: 0.75,
-                  }}
-                  type="button"
-                  size="xs"
-                  onClick={submitPrompt}
-                  disabled={isButtonDisabled}
-                  loading={isLoading}
                 >
-                  <ArrowBendRightUpIcon />
-                </Button>
-              </Flex>
-              <HStack>
-                <Separator flex="1" />
-                <Text
-                  textStyle="xs"
-                  color="fg.muted"
-                  flexShrink="0"
-                  fontStyle="italic"
-                >
-                  ...or try asking about...
-                </Text>
-                <Separator flex="1" />
-              </HStack>
-              <ButtonGroup
-                flexDirection="column"
-                alignItems="start"
-                variant="outline"
-                colorPalette="blue"
-                overflow="auto"
-                maxHeight="272px"
-                size="sm"
-              >
-                {prompts.map((prompt, index) => (
+                  <Textarea
+                    aria-label="Ask a question..."
+                    placeholder="Ask a question..."
+                    fontSize="sm"
+                    size="sm"
+                    autoresize
+                    maxH="10lh"
+                    border="none"
+                    p={0}
+                    value={inputValue}
+                    ref={ref}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyUp={handleKeyUp}
+                    disabled={disabled}
+                    _disabled={{
+                      opacity: 1,
+                    }}
+                    _focus={{
+                      outline: "none",
+                    }}
+                  />
                   <Button
-                    key={index}
-                    bg="white"
-                    _hover={{ bg: "blue.100" }}
-                    onClick={() => handlePromptClick(prompt)}
+                    p="0"
+                    ml="auto"
+                    borderRadius="full"
+                    colorPalette="blue"
+                    bg="blue.900"
+                    _disabled={{
+                      opacity: 0.75,
+                    }}
+                    type="button"
+                    size="xs"
+                    onClick={submitPrompt}
+                    disabled={isButtonDisabled}
+                    loading={isLoading}
                   >
-                    <SparkleIcon />
-                    {prompt}
+                    <ArrowBendRightUpIcon weight="bold" size="10" />
                   </Button>
-                ))}
-              </ButtonGroup>
+                </Flex>
+              </Flex>
+              <Flex display="flex" flexDir="column" gap="6">
+                <HStack>
+                  <Separator flex="1" />
+                  <Text
+                    textStyle="xs"
+                    color="fg.muted"
+                    flexShrink="0"
+                    fontStyle="italic"
+                  >
+                    ...or try asking about...
+                  </Text>
+                  <Separator flex="1" />
+                </HStack>
+                <ButtonGroup
+                  flexDirection="column"
+                  alignItems="start"
+                  colorPalette="blue"
+                  overflow="auto"
+                  maxHeight="272px"
+                  size="xs"
+                  gap={4}
+                >
+                  {prompts.map((prompt, index) => (
+                    <Button
+                      key={index}
+                      color="blue.800"
+                      fontWeight="normal"
+                      bg="white"
+                      rounded="lg"
+                      _hover={{ bg: "blue.800/10" }}
+                      onClick={() => handlePromptClick(prompt)}
+                    >
+                      <SparkleIcon />
+                      {prompt}
+                    </Button>
+                  ))}
+                </ButtonGroup>
+              </Flex>
             </Dialog.Body>
-            <Dialog.Footer>
-              <Dialog.ActionTrigger asChild>
-                <Button variant="outline">Cancel</Button>
-              </Dialog.ActionTrigger>
-              <Button>Save</Button>
-            </Dialog.Footer>
             <Dialog.CloseTrigger asChild>
               <CloseButton onClick={() => setIsOpen(false)} size="sm" />
             </Dialog.CloseTrigger>
