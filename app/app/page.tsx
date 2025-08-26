2404d979cd64815e7105711909f7026f2f221580
LanesGood <Lane@developmentseed.org>
2025-08-26 15:37:56 -0400

    Replace base URL with new threadID when available

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,11 +4,21 @@ import { useEffect } from "react";
 import useChatStore from "@/app/store/chatStore";
 
 export default function Home() {
-  const { reset: resetChatStore } = useChatStore();
+  const { reset: resetChatStore, currentThreadId } = useChatStore();
 
   useEffect(() => {
     resetChatStore();
   }, [resetChatStore]);
 
+  useEffect(() => { // Update URL as soon as threadID is available
+    if (currentThreadId) {
+      const newUrl = `/app/threads/${currentThreadId}`;
+      window.history.replaceState(
+        { ...window.history.state, as: newUrl, url: newUrl },
+        "",
+        newUrl
+      );
+    }
+  }, [currentThreadId]);
   return null; // The layout handles all the UI
 }
