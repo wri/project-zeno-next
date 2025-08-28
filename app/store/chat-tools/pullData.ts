1b9d09f5bf0501019fe70c36140ccd2debb30578
Will Rynearson <will@developmentseed.org>
2025-08-28 09:20:50 +0200

    es-lint ignore

--- app/store/chat-tools/pullData.ts
+++ app/store/chat-tools/pullData.ts
@@ -1,6 +1,11 @@
 import { ChatMessage, StreamMessage } from "@/app/types/chat";
 
 export function pullDataTool(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   streamMessage: StreamMessage,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   addMessage: (message: Omit<ChatMessage, "id">) => void
-) {}
+) {
+  // Tool execution is now handled by the reasoning component
+  // No message needed here
+}
