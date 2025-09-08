99ae4f72ad20db32c6a8e2161d5886ce4264c22c
LanesGood <Lane@developmentseed.org>
2025-09-08 15:54:25 -0400

    TS lint

--- app/utils/formatCharts.tsx
+++ app/utils/formatCharts.tsx
@@ -190,8 +190,10 @@ export const formatYAxisLabel = (value: number, key?: string) => {
   if (key?.toString().toLowerCase() === "year") {
     return value.toString();
   }
-  if (Math.abs(value) < 1000) return value.toLocaleString();
-  if (Math.abs(value) < 1e6) return `${(value / 1e3).toFixed(1)}K`;
-  if (Math.abs(value) < 1e9) return `${(value / 1e6).toFixed(1)}M`;
-  return `${(value / 1e9).toFixed(1)}B`;
+  if (Number(value)) {
+    if (Math.abs(value) < 1000) return value.toLocaleString();
+    if (Math.abs(value) < 1e6) return `${(value / 1e3).toFixed(1)}K`;
+    if (Math.abs(value) < 1e9) return `${(value / 1e6).toFixed(1)}M`;
+    return `${(value / 1e9).toFixed(1)}B`;
+  }
 };
