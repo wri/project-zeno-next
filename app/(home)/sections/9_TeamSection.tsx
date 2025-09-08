18780932bc32c651f4d7f4bbe326fc626df04442
Adam Pain <aj.pain89@gmail.com>
2025-09-08 12:20:03 +0200

    Team: add devseed <3

--- app/(home)/sections/9_TeamSection.tsx
+++ app/(home)/sections/9_TeamSection.tsx
@@ -25,12 +25,12 @@ export default function TeamSection() {
       >
         <Container textAlign="center" maxW="2xl">
           <Heading size={{ base: "3xl", md: "4xl" }}>
-            The team behind Nature Watch
+            The team behind Global Nature Watch
           </Heading>
-          <Text fontSize="lg" mb="4">
-            Nature Watch is the work of World Resources Institute and Land &
-            Carbon Lab, in collaboration with other teams working to shape the
-            future of monitoring research, data and analysis.
+          <Text fontSize="lg" mb="4">         
+            Global Nature Watch is developed by Land & Carbon Lab, a research initiative
+            convened by the Bezos Earth Fund and World Resources Institute, 
+            in collaboration with other teams...
           </Text>
         </Container>
         <Flex
@@ -39,17 +39,20 @@ export default function TeamSection() {
           flexWrap="wrap"
           justifyContent="center"
         >
-          <ChakraLink href="https://www.wri.org/">
+          <ChakraLink href="https://www.wri.org/" target="_blank" rel="noopener noreferrer">
             <Image src="/WRI-logo.svg" alt="WRI Logo" height="64px" />
           </ChakraLink>
-          <ChakraLink href="https://landcarbonlab.org/">
+          <ChakraLink href="https://landcarbonlab.org/" target="_blank" rel="noopener noreferrer">
             <Image src="/LCL-logo.svg" alt="LCL Logo" height="64px" />
           </ChakraLink>
-          <ChakraLink href="https://www.bezosearthfund.org/">
+          <ChakraLink href="https://www.bezosearthfund.org/" target="_blank" rel="noopener noreferrer">
             <Image src="/BEF-logo.png" alt="BEF Logo" height="64px" />
           </ChakraLink>
-          <ChakraLink href="https://www.globalforestwatch.org/">
+          <ChakraLink href="https://www.globalforestwatch.org/" target="_blank" rel="noopener noreferrer">
             <Image src="/GFW-logo.svg" alt="GFW Logo" height="64px" />
+          </ChakraLink>
+          <ChakraLink href="https://developmentseed.org/" target="_blank" rel="noopener noreferrer">
+            <Image src="/developmentseed-logo.svg" alt="DevSeed Logo" height="64px" />
           </ChakraLink>
         </Flex>
       </Container>
