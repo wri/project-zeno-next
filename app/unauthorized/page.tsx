9bf04ce0b98419fcf553a22d135dad968fc01852
Will Rynearson <will@developmentseed.org>
2025-09-11 08:37:31 +0200

    clean up code, update conditional text

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -6,119 +6,99 @@ import Link from "next/link";
 
 const LANDING_PAGE_VERSION = process.env.NEXT_PUBLIC_LANDING_PAGE_VERSION;
 
+const commonStyles = {
+  textShadow: "2px 2px 5px hsla(225, 52%, 11%, 0.75)",
+  color: "fg.inverted",
+};
+
 export default function UnauthorizedPage() {
   return (
     <Box bg="hsla(225, 52%, 11%, 1)">
       <Box
-        width="100%"
-        height="100%"
-        bg="#1d358d"
         position="absolute"
-        top={20}
+        top={20} // adjust this to "lower" the video, but the video top and background colors aren't exactly the same
+        left={0}
+        right={0}
+        bottom={0}
+        bg="#1d358d"
         zIndex="0"
         pointerEvents="none"
-        css={{
-          "& > video": {
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="auto"
+          style={{
             height: "100%",
             width: "100%",
             objectFit: "cover",
             objectPosition: "top",
-          },
-        }}
-      >
-        <video autoPlay loop muted playsInline preload="auto">
-          <source src={"/landing-hero-bg.mp4"} type="video/mp4" />
+          }}
+        >
+          <source src="/landing-hero-bg.mp4" type="video/mp4" />
         </video>
       </Box>
       <Container
         textAlign="center"
         maxW="lg"
-        color="fg.inverted"
+        {...commonStyles}
         py={10}
         display="flex"
         flexDirection="column"
-        gap="4"
-      >
-        <Heading
-          size={{ base: "2xl", md: "4xl" }}
-          textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
-          color="fg.inverted"
-          mb={0}
-          letterSpacing={-2.5}
-        >
-          Global Nature Watch
-        </Heading>
-      </Container>
-      <Box
-        pt={{ base: 14, md: 24 }}
+        gap="6"
+        pt={{ base: 8, md: 12 }}
         pb={{ base: 24, md: 32 }}
         zIndex="10"
         minH={{ base: "none", xl: "45vh" }}
       >
-        <Container
-          textAlign="center"
-          maxW="lg"
-          color="fg.inverted"
-          px={0}
-          display="flex"
-          flexDirection="column"
-          gap="4"
+        <Heading
+          size={{ base: "2xl", md: "4xl" }}
+          {...commonStyles}
+          mb={10}
+          letterSpacing={-2.5}
         >
-          <Heading
-            size={{ base: "3xl", md: "5xl" }}
-            textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
-            color="fg.inverted"
-            mb={0}
-          >
-            {LANDING_PAGE_VERSION === "closed"
-              ? "Early access only"
-              : "Only available to pre-approved email addresses"}
-          </Heading>
-          {LANDING_PAGE_VERSION === "closed" ? (
-            <>
-              <Text
-                fontSize="lg"
-                textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
-                marginBottom={4}
-              >
-                Thank you for your interest in Global Nature Watch!
-              </Text>
-              <Text
-                fontSize="lg"
-                textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
-              >
-                Right now access is limited while we are in closed beta.
-                We&apos;d love for you to be part of what&apos;s next, so join
-                the waitlist to be among the first to know when the tool becomes
-                available.
-              </Text>
-            </>
-          ) : (
-            <Text
-              fontSize="lg"
-              textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
-            >
-              Thank you for showing interest in Global Nature Watch, the tool
-              can currently only be accessed by those invited to early access.
-              If you have not been notified of early access, please join the
-              waitlist.
+          Global Nature Watch
+        </Heading>
+        <Heading size={{ base: "3xl", md: "5xl" }} {...commonStyles} mb={0}>
+          {LANDING_PAGE_VERSION === "closed"
+            ? "Early access only"
+            : "Coming soon"}
+        </Heading>
+        {LANDING_PAGE_VERSION === "closed" ? (
+          <>
+            <Text fontSize="lg" {...commonStyles} marginBottom={4}>
+              Thank you for your interest in Global Nature Watch!
             </Text>
-          )}
-          <Flex justifyContent="center">
+            <Text fontSize="lg" {...commonStyles}>
+              Right now access is limited while we are in closed beta. We&apos;d
+              love for you to be part of what&apos;s next, so join the waitlist
+              to be among the first to know when the tool becomes available.
+            </Text>
+          </>
+        ) : (
+          <Text fontSize="lg" {...commonStyles}>
+            Thank you for creating a Global Nature Watch account. Early access
+            is limited while we scale responsibly. You’re on the waitlist, and
+            we will email you as soon as the tool is ready.
+          </Text>
+        )}
+        <Flex justifyContent="center" gap={4}>
+          <Button
+            asChild
+            size="sm"
+            className="light"
+            variant="solid"
+            colorPalette="primary"
+            rounded="lg"
+          >
+            <Link href="/">Back to homepage</Link>
+          </Button>
+          {LANDING_PAGE_VERSION === "closed" && (
             <Button
               asChild
               size="sm"
-              className="light"
-              variant="solid"
-              colorPalette="primary"
-              rounded="lg"
-            >
-              <Link href="/">Back to homepage</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              ml={4}
               className="light"
               variant="solid"
               colorPalette="primary"
@@ -132,12 +112,9 @@ export default function UnauthorizedPage() {
                 Join waitlist
               </Link>
             </Button>
-          </Flex>
-        </Container>
-      </Box>
-      <Box height="200px">
-        <Text>Hello</Text>
-      </Box>
+          )}
+        </Flex>
+      </Container>
     </Box>
   );
 }
