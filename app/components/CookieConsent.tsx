9776b1eac19f9a9811bab3249917fde6491fc0da
AJ Pain <aj.pain89@gmail.com>
2025-09-12 19:20:19 +0200

    Update banner
    Co-authored-by: Lane Goodman <lane@developmentseed.org>

--- app/components/CookieConsent.tsx
+++ app/components/CookieConsent.tsx
@@ -37,18 +37,40 @@ const CookieConsent = () => {
       <Portal>
         <Box zIndex="1000" position="relative">
           <ActionBar.Positioner>
-            <ActionBar.Content padding="1rem 1rem" display="flex" flexDirection={{ base: 'column', md: 'row' }} alignItems="center" justifyContent="left" gap={4}>
-              <Text>
-                We use cookies to analyze traffic and improve your experience.
-                <br/>
-                Do you consent to the use of analytics cookies?
-              </Text>
+            <ActionBar.Content
+              padding={4}
+              display="flex"
+              flexDirection={{ base: "column", md: "row" }}
+              alignItems="center"
+              justifyContent="space-between"
+              gap={8}
+            >
+              <Flex flexDir="column">
+                <Text>
+                  We use cookies to analyze traffic and improve your experience.
+                  <br />
+                  Do you consent to the use of analytics cookies?
+                </Text>
+                <Text fontSize="xs">
+                  Read our{" "}
+                  <Link
+                    href="https://www.wri.org/about/privacy-policy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    textDecor="underline"
+                    color="primary.solid"
+                  >
+                    Privacy Policy
+                  </Link>
+                </Text>
+              </Flex>
               <Box display="flex" gap={4} flexShrink={0}>
-                <Button variant="outline" onClick={handleReject}>Decline</Button>
-                <Button colorPalette="primary" onClick={handleAccept}>Accept all</Button>
-                <Link href="https://www.wri.org/about/privacy-policy" target="_blank" rel="noopener noreferrer" _hover={{ textDecoration: 'none' }}>
-                  <Button variant="solid">Privacy Policy</Button>
-                </Link>
+                <Button variant="outline" onClick={handleReject}>
+                  Decline
+                </Button>
+                <Button colorPalette="primary" onClick={handleAccept}>
+                  Accept all
+                </Button>
               </Box>
             </ActionBar.Content>
           </ActionBar.Positioner>
