3e02f487855a31dc8730621bb00ba8cbc086276d
LanesGood <Lane@developmentseed.org>
2025-09-15 13:11:45 -0400

    Make disclaimer closable. Remove prompt options once sent. Use prompt store for initial prompts

--- app/components/ChatMessages.tsx
+++ app/components/ChatMessages.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { Fragment, useEffect, useRef } from "react";
-import { Box, Link, Text } from "@chakra-ui/react";
+import { Fragment, useState, useEffect, useRef } from "react";
+import { Box, CloseButton, Link, Text } from "@chakra-ui/react";
 import useChatStore from "@/app/store/chatStore";
 import MessageBubble from "./MessageBubble";
 import Reasoning from "./Reasoning";
 import { InfoIcon } from "@phosphor-icons/react";
+import { usePromptStore } from "../store/promptStore";
 
 const LANDING_PAGE_VERSION = process.env.NEXT_PUBLIC_LANDING_PAGE_VERSION;
 
@@ -16,6 +17,8 @@ const SAMPLE_PROMPTS = [
 function ChatMessages() {
   const containerRef = useRef<HTMLDivElement>(null);
   const { messages, isLoading, sendMessage } = useChatStore();
+  const { prompts } = usePromptStore();
+  const [displayDisclaimer, setDisplayDisclaimer] = useState(true);
 
   // Auto-scroll to bottom when new messages are added or loading state changes
   useEffect(() => {
@@ -59,54 +62,69 @@ function ChatMessages() {
         return (
           <Fragment key={message.id}>
             {/* Disclaimer above first message in closed mode */}
-            {index === 0 && LANDING_PAGE_VERSION !== "public" && (
-              <Box
-                background="secondary.subtle"
-                p={2}
-                rounded="sm"
-                border="1px solid"
-                borderColor="secondary.300"
-                fontSize="xs"
-                display="flex"
-                gap={2}
-                my={4}
-              >
-                <InfoIcon
-                  weight="fill"
-                  style={{ flexShrink: 0 }}
-                  size="16"
-                  fill="var(--chakra-colors-secondary-600)"
-                />
-                <Text>
-                  This is an <strong>experimental preview</strong> of Global
-                  Nature Watch. Our AI assistant may make mistakes, please
-                  verify outputs with primary sources. Feedback is welcome at{" "}
-                  <Link
-                    color="primary.solid"
-                    textDecor="underline"
-                    href="mailto:xyz@landandcarbonlab.org"
-                  >
-                    xyz@landandcarbonlab.org.
-                  </Link>
-                </Text>
-              </Box>
-            )}
+            {index === 0 &&
+              LANDING_PAGE_VERSION !== "public" &&
+              displayDisclaimer && (
+                <Box
+                  background="secondary.subtle"
+                  p={2}
+                  rounded="sm"
+                  border="1px solid"
+                  borderColor="secondary.300"
+                  fontSize="xs"
+                  display="flex"
+                  gap={2}
+                  my={4}
+                  mb={6}
+                >
+                  <InfoIcon
+                    weight="fill"
+                    style={{ flexShrink: 0 }}
+                    size="16"
+                    fill="var(--chakra-colors-secondary-600)"
+                  />
+                  <Text>
+                    This is an <strong>experimental preview</strong> of Global
+                    Nature Watch. Our AI assistant may make mistakes, please
+                    verify outputs with primary sources. Feedback is welcome at{" "}
+                    <Link
+                      color="primary.solid"
+                      textDecor="underline"
+                      href="mailto:xyz@landandcarbonlab.org"
+                    >
+                      xyz@landandcarbonlab.org.
+                    </Link>
+                  </Text>
+                  <CloseButton
+                    size="2xs"
+                    variant="ghost"
+                    colorPalette="secondary"
+                    title="Hide disclaimer"
+                    onClick={() => setDisplayDisclaimer((prev) => !prev)}
+                  />
+                </Box>
+              )}
             <MessageBubble message={message} isConsecutive={isConsecutive} />
             {isLoading && index === lastUserMessageIndex && <Reasoning />}
 
-            {/* Prompt options for first message */}
-            {index === 0 && LANDING_PAGE_VERSION !== "public" && (
-              <Box display="flex" gap={2} flexWrap="nowrap" mb={4}>
-                {SAMPLE_PROMPTS.map((text, i) => (
+            {/* Prompt options for first message, removed when sent */}
+            {messages.length < 2 && LANDING_PAGE_VERSION !== "public" && (
+              <Box display="flex" gap={2} flexWrap="nowrap" mt={6} mb={4}>
+                {prompts.slice(0, 3).map((text, i) => (
                   <Box
                     key={i}
                     p={2}
+                    flex={1}
                     rounded="lg"
                     border="1px solid"
                     borderColor="border.emphasized"
                     fontSize="xs"
                     transition="all 0.24s ease"
-                    _hover={{ cursor: "pointer", bg: "bg.subtle", borderColor: "secondary.solid" }}
+                    _hover={{
+                      cursor: "pointer",
+                      bg: "bg.subtle",
+                      borderColor: "primary.solid",
+                    }}
                     onClick={() => sendMessage(text)}
                   >
                     {text}
