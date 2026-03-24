4b8fc5b97e03cc8eb194b19314d9700944eebcbe
Adam Pain <aj.pain89@gmail.com>
2026-03-24 12:16:54 +0100

    Fix linter

--- app/components/Reasoning.tsx
+++ app/components/Reasoning.tsx
@@ -163,7 +163,7 @@ function Reasoning({
                               <Heading as="h3" size="xs" mb={2} {...props} />
                             </>
                           ),
-                          p: ({ node: _node, ...props }) => <Box mb={2} {...props} />,
+                          p: ({ node: _, ...props }) => <Box mb={2} {...props} />,
                           br: () => <Box h={2} />,
                         }}
                       >
