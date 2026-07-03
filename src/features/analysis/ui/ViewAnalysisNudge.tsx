a66f8143acb83b6431d0981c4eb1da8ef7c3504a
Adam Pain <aj.pain89@gmail.com>
2026-07-03 07:41:02 +0200

    Differentiate generative vs. non-generative nudges

--- src/features/analysis/ui/ViewAnalysisNudge.tsx
+++ src/features/analysis/ui/ViewAnalysisNudge.tsx
@@ -39,7 +39,7 @@ export default function ViewAnalysisNudge({
     });
   };
 
-  const label = `View Analysis for ${suggestion.datasetName} in ${suggestion.area.name}`;
+  const label = `View ${suggestion.datasetName} Analysis in ${suggestion.area.name}`;
 
   return (
     <>
