aa931c6109038cc9374f2254681bfa6312a27822
Adam Pain <aj.pain89@gmail.com>
2026-07-07 13:56:46 +0200

    Hotfix - tidy insight workspace and remove redundant dividers

--- app/components/InsightWorkspace.tsx
+++ app/components/InsightWorkspace.tsx
@@ -261,14 +261,7 @@ export default function InsightWorkspace() {
             }}
           >
             {/* Title row */}
-            <Flex
-              px={4}
-              py={1}
-              justify="space-between"
-              align="flex-start"
-              borderBottom="1px solid"
-              borderColor="#DDE2F5"
-            >
+            <Flex px={4} py={1} justify="space-between" align="flex-start">
               <Heading
                 size="sm"
                 fontWeight="semibold"
@@ -289,13 +282,13 @@ export default function InsightWorkspace() {
 
             {/* Params chips section */}
             {hasChips && paramsExpanded && (
-              <Box px={4} py={2} borderBottom="1px solid" borderColor="#DDE2F5">
+              <Box px={4} py={2}>
                 <AnalysisParamsChips chips={chips} />
               </Box>
             )}
 
             {/* Inner chart card */}
-            <Box px={2} py={2}>
+            <Box px={2} pt={0} pb={2}>
               <WidgetMessage widget={widget} inWorkspace />
             </Box>
           </Box>
