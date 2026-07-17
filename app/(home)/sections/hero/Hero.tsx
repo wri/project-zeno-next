03fad094c9e25ef77d1e8a50146e1140c2177fe3
Adam Pain <aj.pain89@gmail.com>
2026-07-17 11:03:19 +0200

    Update hero text

--- app/(home)/sections/hero/Hero.tsx
+++ app/(home)/sections/hero/Hero.tsx
@@ -192,10 +192,10 @@ export default function LandingHero({
               fontSize="lg"
               textShadow="2px 2px 5px hsla(225, 52%, 11%, 0.75)"
             >
-              Global Nature Watch is an experimental, open, AI-powered system
-              that transforms groundbreaking land monitoring data into
-              intelligence for understanding Earth&rsquo;s landscapes. Test the
-              preview and help shape the future of land monitoring.
+              Global Nature Watch Horizon is an open, AI-powered system that
+              transforms cutting-edge land monitoring data into intelligence for
+              understanding Earth&rsquo;s landscapes. Explore the preview and
+              help shape the future of land monitoring.
             </Text>
           </Container>
           <Container
