32d3b2ddd2c80af3467814463cc53e7a80868db6
Adam Pain <aj.pain89@gmail.com>
2026-07-28 10:56:35 +0200

    hotfix: rebrand feedback link

--- app/constants/preview-content.ts
+++ app/constants/preview-content.ts
@@ -3,7 +3,8 @@ export const PREVIEW_TITLE = "Preview Release";
 export const PREVIEW_BODY =
   "This is an experimental preview of Global Nature Watch Horizon. Results are grounded in trusted datasets, but AI summaries can be incomplete or incorrect. Verify important findings with source data.";
 
-export const PREVIEW_FEEDBACK_EMAIL = "landcarbonlab@wri.org";
+export const PREVIEW_FEEDBACK_FORM =
+  "https://globalnaturewatch.zendesk.com/hc/en-us/requests/new";
 
 export const PREVIEW_HELP_CENTER_URL =
   "https://help.horizon.globalnaturewatch.org/";
