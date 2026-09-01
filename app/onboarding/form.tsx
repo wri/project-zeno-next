a3a55cd338074f95143110d7d83a79abada092a7
Adam Pain <aj.pain89@gmail.com>
2026-09-01 11:27:12 +0200

    Update hint text

--- app/onboarding/form.tsx
+++ app/onboarding/form.tsx
@@ -534,9 +534,9 @@ export default function OnboardingForm({
                   }
                 />
                 <Field.HelperText color="fg.muted" fontSize="xs" mt={1}>
-                  Please input your organization or company&rsquo;s name in
-                  full. For example, enter &ldquo;World Resources
-                  Institute&rdquo; rather than &ldquo;WRI.&rdquo;
+                  Please input your organization or company&rsquo;s name in full
+                  e.g. &ldquo;World Resources Institute&rdquo; rather than
+                  &ldquo;WRI.&rdquo;
                 </Field.HelperText>
               </Field.Root>
             </GridItem>
